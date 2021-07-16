7a25707e327217bf1bf28edeb94e7836fe1f3825
Leandro Jaeger Lafin <leandro.lafin@philips.com>
2021-07-16 19:17:49 -0300

    Configuring router and https route

---
+++ services/router/ssl/README.md
@@ -0,0 +1,7 @@
+# Self signed certificate
+
+The self signed tdc.digi.hero.local certificate in this folder is used for the local development environment. It is generated using the following command:
+
+```bash
+openssl req -x509 -newkey rsa:4096 -out cert.crt -keyout cert.key -days 365 -subj "//C=NL/ST=TdcDemoEnvoy/L=TdcDemoEnvoy/O=TdcDemoEnvoy Name/OU=Org/CN=tdc.digi.hero.local" -nodes -addext "subjectAltName = DNS:tdc.digi.hero.local"
+```
---
+++ setup.ps1
@@ -0,0 +1,31 @@
+function Add-HostEntry {
+  # By Tom Chantler - https://tomssl.com/2019/04/30/a-better-way-to-add-and-remove-windows-hosts-file-entries/
+  param([string]$DesiredIP = "127.0.0.1"
+    , [string]$Hostname = "tomssl.local"
+    , [bool]$CheckHostnameOnly = $false)
+  # Adds entry to the hosts file.
+  #Requires -RunAsAdministrator
+  $hostsFilePath = "$($Env:WinDir)\system32\Drivers\etc\hosts"
+  $hostsFile = Get-Content $hostsFilePath
+
+  Write-Host "About to add $desiredIP for $Hostname to hosts file" -ForegroundColor Gray
+  $escapedHostname = [Regex]::Escape($Hostname)
+  $patternToMatch = If ($CheckHostnameOnly) { ".*\s+$escapedHostname.*" } Else { ".*$DesiredIP\s+$escapedHostname.*" }
+
+  If (($hostsFile) -match $patternToMatch) {
+    Write-Host $desiredIP.PadRight(20, " ") "$Hostname - not adding; already in hosts file" -ForegroundColor DarkYellow
+  }
+  Else {
+    Write-Host $desiredIP.PadRight(20, " ") "$Hostname - adding to hosts file... " -ForegroundColor Yellow -NoNewline
+    Add-Content -Encoding UTF8  $hostsFilePath ("$DesiredIP".PadRight(20, " ") + "$Hostname")
+    Write-Host " done"
+  }
+}
+
+function Add-TrustedCertificate {
+  Write-Host "Adding tdc.digi.hero.local certificate as trusted root" -ForegroundColor Gray
+  Import-Certificate -FilePath .\services\router\ssl\cert.crt -CertStoreLocation cert:\CurrentUser\Root
+} 
+
+Add-HostEntry -DesiredIP 127.0.0.1 -Hostname tdc.digi.hero.local
+Add-TrustedCertificate
